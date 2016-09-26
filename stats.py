7cb72c5fca8bdd669afb50fc82f6b1e84fdf4156
Jared Drayton <jareddrayton@users.noreply.github.com>
2016-09-26 15:48:54 +0100

    Write Voiced Boolean to CSV
    Write Voiced Boolean to CSV

--- stats.py
+++ stats.py
@@ -63,10 +63,15 @@ def formant_plot_animated():
 def write_formants(name, directory, currentgeneration, individualfrequencies, fitness, voiced):
 
 	
+	csvdata = individualfrequencies
+	csvdata.append(fitness)
+	csvdata.append(voiced)
+	print(csvdata)
 
 	with open('%s\\Generation%d\\Stats.csv' % (directory, currentgeneration), 'a') as csvfile:
 	
 		spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		spamwriter.writerow(csvdata)
 
 
 #################################################################################################################
