83a1cec878ca9f67fcfb683b79728ef2ae859ce5
jareddrayton <jareddrayton@users.noreply.github.com>
2017-07-28 16:48:11 +0100

    Updated spacing as per PEP 8
    Signed-off-by: jareddrayton <jareddrayton@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ import matplotlib.pyplot as plt
 
 import praatcontrol, fitnessfunction, genop, stats
 
-
 # Key Word Arguments HERE
 
-script, soundfile, generations, generationsize, mutationprobability, standarddeviation, parallel, ff, metric, identifier = argv 
+script, soundfile, generations, generationsize, mutationprobability, standarddeviation, parallel, ff, metric, identifier = argv
 
 # Convert arguments from strings to integers and floats 
 
@@ -27,8 +26,9 @@ random.seed(int(identifier))
 
 currentgeneration = 0
 
-directory = "%s Gen %d Pop %d Mut %g SD %g %s %s %s" % (soundfile, generations-1,
-generationsize, mutationprobability, standarddeviation, ff, metric, identifier)
+directory = "%s Gen %d Pop %d Mut %g SD %g %s %s %s" % (soundfile, generations - 1,
+                                                        generationsize, mutationprobability, standarddeviation, ff,
+                                                        metric, identifier)
 
 os.mkdir(directory)
 
@@ -39,64 +39,65 @@ targetrms = praatcontrol.get_target_RMS(soundfile)
 
 length = "0.5"
 
+
 #################################################################################################################
 #################################################################################################################
 
 class Individual:
-
     def __init__(self, name):
-        
+
         self.name = name
-        
+
         self.values = []
 
         self.parameters = ['Interarytenoid',
-                      'Cricothyroid',
-                      'Vocalis',
-                      'Thyroarytenoid',
-                      'PosteriorCricoarytenoid',
-                      'LateralCricoarytenoid',
-                      'Stylohyoid',
-                      'Sternohyoid',
-                      'Thyropharyngeus',
-                      'LowerConstrictor',
-                      'MiddleConstrictor',
-                      'UpperConstrictor',
-                      'Sphincter',
-                      'Hyoglossus',
-                      'Styloglossus',
-                      'Genioglossus',
-                      'UpperTongue',
-                      'LowerTongue',
-                      'TransverseTongue',
-                      'VerticalTongue',
-                      'Risorius',
-                      'OrbicularisOris',
-                      'TensorPalatini',
-                      'Masseter',
-                      'Mylohyoid',
-                      'LateralPterygoid',
-                      'Buccinator']
+                           'Cricothyroid',
+                           'Vocalis',
+                           'Thyroarytenoid',
+                           'PosteriorCricoarytenoid',
+                           'LateralCricoarytenoid',
+                           'Stylohyoid',
+                           'Sternohyoid',
+                           'Thyropharyngeus',
+                           'LowerConstrictor',
+                           'MiddleConstrictor',
+                           'UpperConstrictor',
+                           'Sphincter',
+                           'Hyoglossus',
+                           'Styloglossus',
+                           'Genioglossus',
+                           'UpperTongue',
+                           'LowerTongue',
+                           'TransverseTongue',
+                           'VerticalTongue',
+                           'Risorius',
+                           'OrbicularisOris',
+                           'TensorPalatini',
+                           'Masseter',
+                           'Mylohyoid',
+                           'LateralPterygoid',
+                           'Buccinator']
 
         self.fitness = 0
         self.fitnessscaled = 0
-        
+
         if currentgeneration == 0:
             self.initialise_values()
-            
-    def initialise_values(self): 
+
+    def initialise_values(self):
         """ Method for Initialising candidates with random values """
 
         self.values = [round(random.uniform(-1, 1), 1) for x in range(len(self.parameters))]
-    
+
         return self.values
 
     def create_artword(self):
-    
-        self.artword = open("{}/Generation{!s}/Individual{!s}.praat".format(directory, currentgeneration, self.name), "w")
-        
+
+        self.artword = open("{}/Generation{!s}/Individual{!s}.praat".format(directory, currentgeneration, self.name),
+                            "w")
+
         self.artword.write('Create Speaker... Robovox Male 2\r\n')
-        self.artword.write('Create Artword... Individual'+ self.name + ' ' + length + '\r\n')
+        self.artword.write('Create Artword... Individual' + self.name + ' ' + length + '\r\n')
         self.artword.write('Set target... 0.0  0.07  Lungs\r\n')
         self.artword.write('Set target... 0.04  0.0  Lungs\r\n')
         self.artword.write('Set target... %s   0.0  Lungs\r\n' % length)
@@ -104,12 +105,12 @@ class Individual:
         self.artword.write('Set target... ' + length + ' 1 LevatorPalatini\r\n')
 
         for i in range(len(self.parameters)):
-            
             self.artword.seek(0, 2)
             self.artword.write('Set target... 0.0 ' + str(self.values[i]) + ' ' + self.parameters[i] + '\r\n')
-            self.artword.write('Set target... ' + length + ' ' + str(self.values[i]) + ' ' + self.parameters[i] + '\r\n')
-        
-        self.artword.write('select Artword Individual'+ self.name + '\r\n')
+            self.artword.write(
+                'Set target... ' + length + ' ' + str(self.values[i]) + ' ' + self.parameters[i] + '\r\n')
+
+        self.artword.write('select Artword Individual' + self.name + '\r\n')
         self.artword.write('plus Speaker Robovox\r\n')
         self.artword.write('To Sound... 22500 25    0 0 0    0 0 0   0 0 0\r\n')
         self.artword.write('''nowarn do ("Save as WAV file...", "Individual''' + self.name + '''.wav")\r\n''')
@@ -125,18 +126,16 @@ class Individual:
         self.artword.write('To Intensity: 100, 0, "yes"\r\n')
         self.artword.write('Get standard deviation: 0, 0\r\n')
         self.artword.write('''appendFile ("intensity''' + self.name + '''.txt", info$ ())\r\n''')
-        
+
         self.artword.close()
 
-
-
     def evaluate_fitness(self):
-        
 
-        self.intensity = praatcontrol.get_individual_intensity(self.name, directory, currentgeneration,targetintensity)
-        self.rms = praatcontrol.get_individual_RMS(self.name,directory,currentgeneration,targetrms)
-        self.formants, self.voiced = praatcontrol.get_individual_frequencies(self.name,directory,currentgeneration)
-        
+        #This method should pullin
+
+        self.intensity = praatcontrol.get_individual_intensity(self.name, directory, currentgeneration, targetintensity)
+        self.rms = praatcontrol.get_individual_RMS(self.name, directory, currentgeneration, targetrms)
+        self.formants, self.voiced = praatcontrol.get_individual_frequencies(self.name, directory, currentgeneration)
 
         if ff == "A1":
             self.fitness = fitnessfunction.fitness_a1(self.formants, self.voiced, targetfrequencies, metric)
@@ -144,7 +143,6 @@ class Individual:
             self.fitness = fitnessfunction.fitness_a2(self.formants, self.voiced, targetfrequencies, metric)
         elif ff == "A3":
             self.fitness = fitnessfunction.fitness_a3(self.formants, self.voiced, targetfrequencies, metric)
-
 
         print("Individual ", self.name)
         print("Voiced                :", self.voiced)
@@ -154,12 +152,13 @@ class Individual:
         print("RMS Coefficient       :", self.rms)
         print("Fitness * RMS         :", self.fitness * self.rms)
 
-        #self.fitness = self.fitness * self.rms
+        # self.fitness = self.fitness * self.rms
         self.fitness = self.fitness * self.intensity
-        #self.fitness = self.fitness * ((self.rms + self.intensity) / 2.0)
-        
-        stats.write_formants(self.name, directory, currentgeneration, 
-            self.formants, self.fitness, self.voiced)
+        # self.fitness = self.fitness * ((self.rms + self.intensity) / 2.0)
+
+        stats.write_formants(self.name, directory, currentgeneration,
+                             self.formants, self.fitness, self.voiced)
+
 
 #################################################################################################################
 #################################################################################################################
@@ -172,9 +171,9 @@ averagefitness = []
 minimumfitness = []
 
 for i in range(generations):
-    
+
     os.mkdir(directory + "/Generation%d" % currentgeneration)
-    
+
     if currentgeneration == 0:
         for name in keys:
             population[name] = Individual(name)
@@ -197,14 +196,15 @@ for i in range(generations):
     for name in keys:
         listfitness.append(population[name].fitness)
 
-    averagefitness.append(sum(listfitness)/len(listfitness))
+    averagefitness.append(sum(listfitness) / len(listfitness))
     minimumfitness.append(min(listfitness))
 
     # genop.lin_rank(population, keys)
-    
-    genop.fitness_proportional(population, keys)        
-    
-    genop.mutation(population, keys,mutationprobability,standarddeviation)
+
+    genop.fitness_proportional(population, keys)
+
+    genop.mutation(population, keys, mutationprobability, standarddeviation)
+
 
 #################################################################################################################
 #################################################################################################################
@@ -215,18 +215,19 @@ def statistics():
     with open("{}/Mean.txt".format(directory), "w") as mean:
         for item in averagefitness:
             mean.write("{!s}\r\n".format(item))
-    
+
     with open("{}/Minimum.txt".format(directory), "w") as minimum:
         for item in minimumfitness:
             minimum.write('{!s}\r\n'.format(item))
 
-    plt.plot(averagefitness, 'k', label = 'Mean Fitness')
-    plt.plot(minimumfitness, 'k--', label = 'Minimum Fitness')
+    plt.plot(averagefitness, 'k', label='Mean Fitness')
+    plt.plot(minimumfitness, 'k--', label='Minimum Fitness')
     plt.axis([0, generations - 1, 0, max(averagefitness)])
     plt.xlabel('Generations')
     plt.ylabel('Fitness')
     plt.legend()
     plt.savefig("{}/Performance Graph".format(directory))
+
 
 statistics()
 
