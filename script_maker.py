013a53f2a1c418cd096214df92b99fbe00928732
jareddrayton <jareddrayton@users.noreply.github.com>
2017-10-04 15:42:44 +0100

    Added a basic elitism operator.
    Signed-off-by: jareddrayton <jareddrayton@users.noreply.github.com>

--- script_maker.py
+++ script_maker.py
@@ -14,31 +14,29 @@ vowels = os.listdir(path)
 
 
 # choose from hz, mel, cents, bark, erb, brito
-features = ["hz", "mel"]# "cents", "bark", "erb", "brito"]
+features = ["hz", "mel", "cents", "bark", "erb", "brito"]
 
 # choose from "SSD", "SAD", "EUC"
 distance_metrics = ["SSD"]
 
-# choose from "both", "rms", "intensity", 
+# choose from "both", "rms", "intensity",
 loudness = ["both"]
-
 
 # specify the ga parameters
 
 no_of_runs = 2
 
-gen_size = '4 '
-pop_size = '10 '
+gen_size = '15 '
+pop_size = '100 '
 mutation = '0.2 '
 standard_dev = '0.2'
 
-GA_params = gen_size + pop_size + mutation + standard_dev 
+GA_params = gen_size + pop_size + mutation + standard_dev
 
 for v in vowels:
 	for f in features:
 		for d in distance_metrics:
 			for l in loudness:
 				for n in range(no_of_runs):
-					
 					with open('batch.cmd', 'a') as t:
 						t.write("python main.py {!s} {!s} P {!s} {!s} {!s} {!s}\n".format(v, GA_params, f, d, l, n))
