2a8183678ecd42c8c28051a3f0713d05a282d8f8
unknown <jareddrayton@users.noreply.github.com>
2018-02-06 20:28:21 +0000

    Added box plot to prototyping

---
+++ Prototyping/box_plot.py
@@ -0,0 +1,27 @@
+import csv
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# https://matplotlib.org/devdocs/api/_as_gen/matplotlib.axes.Axes.violinplot.html
+# read in the mean fitness of each generation for each run and average it.
+
+my_data = []
+
+for i in range(4):
+    
+    directory = "vowel-01.wav Gen 10 Pop 20 Mut 0.2 Sd 0.2 cent SSD none {!s}".format(i)
+
+    column = []
+    
+    with open('{}/Stats.csv'.format(directory)) as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            print(row[0])
+            column.append(float(row[0]))
+
+    my_data.append(column)   
+    print(my_data)
+
+plt.boxplot(my_data)
+plt.show()
