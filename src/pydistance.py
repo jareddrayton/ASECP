49bb0d3d37db0c1de4095b157d117a0126fdbc64
jareddrayton <jareddrayton@users.noreply.github.com>
2021-03-09 21:19:21 +0000

    Remove unused brito function

--- src/pydistance.py
+++ src/pydistance.py
@@ -4,125 +4,95 @@ in the math.NET project http://numerics.mathdotnet.com/Distance.html """
 import math
 
 def SAD(x, y):
-	""" Sum of the Absolute Difference
-	:input x: a one dimensional list containing integers or floats.
-	:input y: a one dimensional list containing integers or floats. 
+    """ Sum of the Absolute Difference
+    :input x: a one dimensional list containing integers or floats.
+    :input y: a one dimensional list containing integers or floats. 
 
-	:returns: the sum of the absolute differences of each element
-	"""
+    :returns: the sum of the absolute differences of each element
+    """
 
-	total = []
+    total = []
 
-	for i in range(len(x)):
-		total.append(x[i] - y[i])
+    for i in range(len(x)):
+        total.append(x[i] - y[i])
 
-	total = map(abs, total)
+    total = map(abs, total)
 
-	return sum(total)
+    return sum(total)
 
 
 def SSD(x,y):
-	""" Sum of the Squared Difference
-	:input x: a one dimensional list.
-	:input y: a one dimensional list. 
+    """ Sum of the Squared Difference
+    :input x: a one dimensional list.
+    :input y: a one dimensional list. 
 
-	:returns: the sum of the absolute differences of each element
-	"""
+    :returns: the sum of the absolute differences of each element
+    """
 
-	total = []
+    total = []
 
-	for i in range(len(x)):
-		total.append((x[i] - y[i]) ** 2)
+    for i in range(len(x)):
+        total.append((x[i] - y[i]) ** 2)
 
-	return sum(total)
+    return sum(total)
 
 
 def MAE(x,y):
-	""" Mean-Absolute Error
-	:input x: a one dimensional list containing integers or floats.
-	:input y: a one dimensional list containing integers or floats. 
+    """ Mean-Absolute Error
+    :input x: a one dimensional list containing integers or floats.
+    :input y: a one dimensional list containing integers or floats. 
 
-	:returns: the sum of the absolute differences of each element
-	"""
+    :returns: the sum of the absolute differences of each element
+    """
 
-	total = []
+    total = []
 
-	for i in range(len(x)):
-		total.append(x[i] - y[i])
+    for i in range(len(x)):
+        total.append(x[i] - y[i])
 
-	total = map(abs, total)
+    total = map(abs, total)
 
-	return sum(total) // len(x)
+    return sum(total) // len(x)
 
 
 def MSE(x,y):
-	""" Mean-Squared Error Distance
-	:input x: a one dimensional list.
-	:input y: a one dimensional list. 
+    """ Mean-Squared Error Distance
+    :input x: a one dimensional list.
+    :input y: a one dimensional list. 
 
-	:returns: the sum of the absolute differences of each element
-	"""
+    :returns: the sum of the absolute differences of each element
+    """
 
-	total = []
+    total = []
 
-	for i in range(len(x)):
-		total.append((x[i] - y[i]) ** 2)
+    for i in range(len(x)):
+        total.append((x[i] - y[i]) ** 2)
 
-	return sum(total) // len(x)
+    return sum(total) // len(x)
 
 
 def EUC(x,y):
-	""" Euclidean distance
+    """ Euclidean distance
 
-	:returns: the sum of the absolute differences of each element
-	"""
+    :returns: the sum of the absolute differences of each element
+    """
 
-	total = []
+    total = []
 
-	for i in range(len(x)):
-		total.append((x[i] - y[i]) ** 2)
+    for i in range(len(x)):
+        total.append((x[i] - y[i]) ** 2)
 
-	return math.sqrt(sum(total))
+    return math.sqrt(sum(total))
 
 def CAN(x,y):
-	""" Canberra Distance
+    """ Canberra Distance
 
-	:
-	"""
+    :
+    """
 
-	total = []
+    total = []
 
-	for i in range(len(x)):
-		total.append(abs(x[i]-y[i]) / (abs(x[i]) + abs(y[i])))
+    for i in range(len(x)):
+        total.append(abs(x[i]-y[i]) / (abs(x[i]) + abs(y[i])))
 
-	return sum(total)
-
-
-def BRITO(x,y):
-
-	weights_osix = [90, 60, 24, 12, 1]
-	weights_oseven = [40, 30, 15, 10, 1]
-
-	total = []
- 	
-	formant_distances = []
-
-	for i in range(len(formants)):
-		formant_distances.append(weights_oseven[i] * (abs((targetformants[i] - formants[i])) / targetformants[i]))
-	
-	
-	for i in range(len(x)):
-		total.append(x[i] - y[i])
-
-	total = map(abs, total)
-
-	return sum(total)
-
-
-
-# y = [495.0, 1627.0, 2267.0]
-# x = [319.0, 1944.0, 2700.0]
-# y = [10000, 10000, 10000]
-
-# print CAN(x,y)
-# print SSD(x,y)
+    return sum(total)
