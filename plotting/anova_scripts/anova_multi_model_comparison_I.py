9a176c8efbfcdec5ea9f2dc2593d18667e0535ee
jareddrayton <jareddrayton@users.noreply.github.com>
2022-07-10 15:37:15 +0100

    Add plotting scripts.

--- plotting/anova_scripts/anova_multi_model_comparison_I.py
+++ plotting/anova_scripts/anova_multi_model_comparison_I.py
@@ -8,14 +8,10 @@ import seaborn as sns
 
 from CONSTANTS import target_vowel_data
 
-# target_vowel_data = {'Primary1.wav': [277, 1947, 2629],
-#                      'Primary4.wav': [639, 1145, 2195],
-#                      'Primary8.wav': [335, 936, 2200]}
-
-glob_string = 'C:\\Users\\Jazz\\VSCODE\\Repo\\ASECP\\data\\distance_metric_feature_representation\\Primary*'
+glob_string = 'C:\\Users\\Jazz\\VSCODE\\Repo\\ASECP\\data\\multi_model_comparison_phase_i\\Primary*'
 paths = glob.glob(glob_string)
 
-columns = ['vowel', 'run', 'dm', 'fr', 'f1', 'f2', 'f3']
+columns = ['vowel', 'run', 'synthesiser', 'f1', 'f2', 'f3']
 
 # A list to store each dictioniary that represents a row.
 store_rows = []
@@ -31,8 +27,7 @@ for path in paths:
     final_generation = arguments['generation_size']
     vowel = arguments['soundfile']
     identifier = arguments['identifier']
-    dm = arguments['distance_metric']
-    fr = arguments["formant_repr"]
+    synthesiser = arguments['synthesiser']
 
     with open(path + f'\\Generation{final_generation}\\individual_info_table.csv', 'r') as f:
         new_data = csv.reader(f, delimiter=' ')
@@ -42,26 +37,22 @@ for path in paths:
                 best_individual_data = row
 
     formants_ind = [float(x.strip()) for x in best_individual_data[5:8]]
-    formants_abs = list(map(lambda x, y: abs(x-y), formants_ind, target_vowel_data[vowel]))
-    row_data_formatted = [vowel, identifier, dm, fr] + formants_abs
+    formants_abs = list(map(lambda x, y: abs(x-y), formants_ind, target_vowel_data[vowel][:3]))
+    row_data_formatted = [vowel, identifier, synthesiser] + formants_abs
 
     row_data = dict(zip(columns, row_data_formatted))
+
     store_rows.append(row_data)
 
 
 new_df = pd.DataFrame.from_records(store_rows)
 
-def create_plots(new_df):
-    formants = ["f1", "f2", "f3"]
-    for formant in formants:
-        sns.set_theme(style="whitegrid")
-        sns.set(font_scale=1.75)
-        g = sns.catplot(x="dm", y=formant, hue="fr", col="vowel",
-                        capsize=.2, palette="YlGnBu_d", height=12, aspect=0.4,
-                        kind="point", data=new_df, dodge=False)
-        g.despine(left=True)
 
-    plt.show()
+sns.set_theme(style="whitegrid")
+sns.set(font_scale=1.75)
+g = sns.catplot(x="vowel", y='f3',  hue="synthesiser",
+                capsize=0.8, palette="YlGnBu_d",
+                kind="point", data=new_df, join=False, dodge=True, height=10, aspect=2.0)
+g.despine(left=True)
 
-
-create_plots(new_df)
+plt.show()
---
+++ plotting/anova_scripts/anova_weightings.py
@@ -0,0 +1,65 @@
+import csv
+import glob
+import json
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+target_vowel_data = {'Primary1.wav': [277, 1947, 2629],
+                     'Primary4.wav': [639, 1145, 2195],
+                     'Primary8.wav': [335, 936, 2200]}
+
+glob_string = 'C:\\Users\\Jazz\\VSCODE\\Repo\\ASECP\\data\\weightings\\Primary*'
+paths = glob.glob(glob_string)
+
+glob_string2 = 'C:\\Users\\Jazz\\VSCODE\\Repo\\ASECP\\data\\distance_metric_feature_representation\\Primary*.wav.PRT.Gen20.Pop150.Mut0.1.Sd0.2.cent.EUC*'
+paths2 = glob.glob(glob_string2)
+
+paths += paths2
+
+
+columns = ['vowel', 'run', 'weight_features', 'formant', 'frequency']
+
+# A list to store each dictioniary that represents a row.
+store_rows = []
+
+for path in paths:
+    # read best
+    with open(path + '\\Best.txt', 'r') as f:
+        best_individual_id = int(f.readlines()[-1])
+
+    with open(path + '\\arguments.json', 'r') as f:
+        arguments = json.loads(f.read())
+
+    final_generation = arguments['generation_size']
+    vowel = arguments['soundfile']
+    identifier = arguments['identifier']
+    el = arguments['weight_features']
+
+    with open(path + f'\\Generation{final_generation}\\individual_info_table.csv', 'r') as f:
+        new_data = csv.reader(f, delimiter=' ')
+
+        for i, row in enumerate(new_data):
+            if i == best_individual_id:
+                best_individual_data = row
+
+    formants_ind = [float(x.strip()) for x in best_individual_data[5:8]]
+    formants_abs = list(map(lambda x, y: abs(x-y), formants_ind, target_vowel_data[vowel]))
+    row_data_formatted = [vowel, identifier, el]
+    formants = ['f1', 'f2', 'f3']
+    for label, value in zip(formants, formants_abs):
+        row_data = dict(zip(columns, row_data_formatted + [label, value]))
+        store_rows.append(row_data)
+
+
+new_df = pd.DataFrame.from_records(store_rows)
+
+sns.set_theme(style="whitegrid")
+sns.set(font_scale=1.75)
+g = sns.catplot(x="vowel", y='frequency',  hue="weight_features", col="formant",
+                capsize=.2, palette="YlGnBu_d", height=20, aspect=0.4,
+                kind="point", data=new_df, dodge=False)
+g.despine(left=True)
+
+plt.show()
